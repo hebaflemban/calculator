92bd6742e72740ee42156af1ceae2b2d3a537320
Heba <36724067+heba@users.noreply.github.com>
2020-08-16 15:14:54 +0300

    fixing the calculator function

--- calculator.py
+++ calculator.py
@@ -1,38 +1,41 @@
 
 def main():
+	while True:
+		num1 = input("Enter the first number: ")
+		try:
+			num1 = int(num1)
+		except ValueError:
+			print("Not a valid number ")
+			break
+		else:
+			pass
 
-	num1 = input("Enter the first number: ")
-	try:
-		num1 = int(num1)
-	except ValueError:
-		print("Not a valid number ")
-	else:
-		pass
+		num2 = input("Enter the second number: ")
+		try:
+			num2 = int(num2)
+		except ValueError:
+			print("Not a valid number ")
+			break
+		else:
+			pass
 
-	num2 = input("Enter the second number: ")
-	try:
-		num2 = int(num2)
-	except ValueError:
-		print("Not a valid number ")
-	else:
-		pass
-
-	opr = input("Choose an operation from the following (+, -, /, *): ")
+		opr = input("Choose an operation from the following (+, -, /, *): ")
 
 
-	if opr == "+" :
-		result = int(num1) + int(num2)
-	elif opr == "-":
-		result = int(num1) - int(num2)
-	elif opr == "/":
-		result = int(num1) / int(num2)
-	elif opr=="*":
-		result = int(num1) * int(num2)
-	else:
+		if opr == "+" :
+			result = int(num1) + int(num2)
+		elif opr == "-":
+			result = int(num1) - int(num2)
+		elif opr == "/":
+			result = int(num1) / int(num2)
+		elif opr=="*":
+			result = int(num1) * int(num2)
+		else:
 			print("The operation entered is not valid")
+			break
 
 
-	print (result)
+		print (result)
 
 
 
