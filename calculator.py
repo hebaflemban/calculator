d27f1059f5f3f8d36e0a69081287b42449179e64
Heba <36724067+heba@users.noreply.github.com>
2020-08-16 15:08:34 +0300

    add the calculator funtion

--- calculator.py
+++ calculator.py
@@ -1,8 +1,39 @@
 
 def main():
-	#write your code here
-	pass
-	
+
+	num1 = input("Enter the first number: ")
+	try:
+		num1 = int(num1)
+	except ValueError:
+		print("Not a valid number ")
+	else:
+		pass
+
+	num2 = input("Enter the second number: ")
+	try:
+		num2 = int(num2)
+	except ValueError:
+		print("Not a valid number ")
+	else:
+		pass
+
+	opr = input("Choose an operation from the following (+, -, /, *): ")
+
+
+	if opr == "+" :
+		result = int(num1) + int(num2)
+	elif opr == "-":
+		result = int(num1) - int(num2)
+	elif opr == "/":
+		result = int(num1) / int(num2)
+	elif opr=="*":
+		result = int(num1) * int(num2)
+	else:
+			print("The operation entered is not valid")
+
+
+	print (result)
+
 
 
 
